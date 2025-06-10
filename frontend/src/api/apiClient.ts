04390f16df4f55850365193cfc3ac0d6aefe178a
woodentouch <woodentouchofficial@gmail.com>
2025-06-10 20:20:56 +0200

    fix: handle plain responses

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -29,19 +29,22 @@ axiosInstance.interceptors.request.use(
 
 // 响应拦截
 axiosInstance.interceptors.response.use(
-	(res: AxiosResponse<Result>) => {
-		if (!res.data) throw new Error(t("sys.api.apiRequestFailed"));
+        (res: AxiosResponse<Result>): any => {
+                if (!res.data) throw new Error(t("sys.api.apiRequestFailed"));
 
-		const { status, data, message } = res.data;
-		// 业务请求成功
-		const hasSuccess = data && Reflect.has(res.data, "status") && status === ResultEnum.SUCCESS;
-		if (hasSuccess) {
-			return data;
-		}
+                if (Reflect.has(res.data, "status")) {
+                        const { status, data, message } = res.data as Result;
+                        const hasSuccess = status === ResultEnum.SUCCESS;
+                        if (hasSuccess) {
+                                return data as unknown;
+                        }
 
-		// 业务请求错误
-		throw new Error(message || t("sys.api.apiRequestFailed"));
-	},
+                        throw new Error(message || t("sys.api.apiRequestFailed"));
+                }
+
+                // Fallback for plain responses
+                return res.data as unknown;
+        },
 	(error: AxiosError<Result>) => {
 		const { response, message } = error || {};
 
