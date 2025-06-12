335dc400494ef474541288baa7870939943e0fc9
woodentouch <woodentouchofficial@gmail.com>
2025-06-12 17:04:36 +0200

    fix(json): handle panier serialization

--- backend/src/main/java/com/wooden/project/model/PanierItem.java
+++ backend/src/main/java/com/wooden/project/model/PanierItem.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "panier_items")
@@ -17,6 +18,7 @@ public class PanierItem {
 
     @ManyToOne
     @JoinColumn(name = "id_panier", referencedColumnName = "id_panier")
+    @JsonBackReference
     private Panier panier;
 
     @ManyToOne
